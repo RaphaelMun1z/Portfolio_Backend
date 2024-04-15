faa35471e41de63d90b07a66cd4079b4db765776
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-04-15 20:24:08 -0300

    Cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors({
     credentials: true,
     origin: [
-        "http://localhost:5000/",
+        "http://localhost:5173"
     ]
 }))
 
