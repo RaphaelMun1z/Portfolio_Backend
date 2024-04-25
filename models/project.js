0d395e65bf5797ed90f9a3cd595b6de38256d171
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-04-25 19:57:11 -0300

    Att

--- models/project.js
+++ models/project.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'projectId',
         onDelete: 'CASCADE',
       });
-    }
+    } 
   }
   Project.init({
     name: DataTypes.STRING,
