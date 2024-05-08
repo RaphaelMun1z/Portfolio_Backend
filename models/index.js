e66a6cb27395c1a5de61ee32b7147dc5e9b3372a
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-07 23:30:54 -0300

    a

--- models/index.js
+++ models/index.js
@@ -14,9 +14,11 @@ import mysql2 from 'mysql2';
 let sequelize;
 if (config.use_env_variable) {
   config.dialectModule = mysql2;
+  console.log(config.dialectModule)
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   config.dialectModule = mysql2;
+  console.log(config.dialectModule)
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
