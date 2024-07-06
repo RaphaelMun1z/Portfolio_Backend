38887f6e39c59520dae9311010ff12066797127a
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-06 11:55:56 -0300

    Fix: mysql import

--- models/index.js
+++ models/index.js
@@ -9,14 +9,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
-//import mysql2 from 'mysql2';
+import mysql2 from 'mysql2';
 
 let sequelize;
 if (config.use_env_variable) {
-  //config.dialectModule = mysql2;
+  config.dialectModule = mysql2;
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  //config.dialectModule = mysql2;
+  config.dialectModule = mysql2;
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
